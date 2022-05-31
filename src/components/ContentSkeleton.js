7b100d721abc3ea26a119531dd53329bc7480621
serifeyaman <serifeyaman@ozbelsan.com.tr>
2022-05-31 21:41:55 +0300

    task3 sayfasi için skeleton eklendi

---
+++ src/components/ContentSkeleton.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import ContentLoader from 'react-content-loader'
+
+const ContentSkeleton = () => {
+    return (
+        <ContentLoader
+            speed={2}
+            width={window.innerWidth}
+            height={"25%"}
+            viewBox={`0 0 ${window.innerWidth} 160`}
+            backgroundColor="red"
+            foregroundColor="blue"
+            backgroundColor="#f0f0f0"
+            foregroundColor="#dedede"
+        >
+            <rect x="1%" y="2%" rx="3" ry="3" width="14%" height="55%" />
+
+            <rect x="17%" y="5%" rx="3" ry="3" width="15%" height="15%" />
+            <rect x="17%" y="28%" rx="2" ry="2" width="18%" height="25%" />
+
+            <rect x="78%" y="5%" rx="3" ry="3" width="6%" height="7%" />
+            <rect x="78%" y="15%" rx="3" ry="3" width="6%" height="7%" />
+            <rect x="72%" y="30%" rx="3" ry="3" width="12%" height="20%" />
+
+        </ContentLoader>
+    )
+}
+
+export default ContentSkeleton
--- src/pages/Task3.js
+++ src/pages/Task3.js
@@ -6,11 +6,16 @@ import { Card, Col, Row } from 'reactstrap'
 import { getOfferCountCase3, getOffersCase_3 } from 'redux/insuranceOffers/action'
 import { sortedArrayByPrice } from 'utility/ArraySort'
 import "assets/css/offersCount.css"
+import ContentSkeleton from 'components/ContentSkeleton'
+
+export const SkeletonShow = ({ offerCount, offerList }) => {
+  var contentCount = offerCount - offerList?.length;
+  return Array.from({length: contentCount})?.map((item, key) => <ContentSkeleton key={key} />)
+}
 
 const Task3 = () => {
 
   const [offerList, setOfferList] = useState([])
-
   const { case3OfferData, case3LoadingData, offerCount } = useSelector(state => state.OffersReducer)
   const dispatch = useDispatch()
 
@@ -26,22 +31,17 @@ const Task3 = () => {
     offerList?.length < offerCount && setOfferList([...offerList, case3OfferData])
   }, [case3OfferData])
 
-
-  // useEffect(() => {
-  //   (!case3LoadingData && offerList?.length < offerCount) && setOfferList([...offerList, case3OfferData])
-  // }, [case3OfferData || case3LoadingData])
-
   return (
     <div className='main-div'>
       <div style={{ height: '85%', width: '85%', overflow: 'scroll', position: 'relative' }}>
         <Card className='mt-3 mb-3' className='count-card'>
           <Row className='count-card-row'>
-            <Col xl="3" lg="3" md="5" sm="5" xs="6" className='count-col' style={{justifyContent:'flex-start'}}>
+            <Col xl="3" lg="3" md="5" sm="5" xs="6" className='count-col' style={{ justifyContent: 'flex-start' }}>
               <h5 style={{ color: '#8aa2de', margin: 0 }}>Gösterilen Teklif Sayısı :</h5>
             </Col>
             <Col xl="2" lg="2" md="4" sm="4" xs="6" className='count-col'>
               <span className='countBadge'>
-                <p style={{ color: 'white', margin: 0, fontWeight:'700' }}>{offerList?.length} {"/"} {offerCount}</p>
+                <p style={{ color: 'white', margin: 0, fontWeight: '700' }}>{offerList?.length} {"/"} {offerCount}</p>
               </span>
             </Col>
           </Row>
@@ -50,11 +50,15 @@ const Task3 = () => {
           case3LoadingData ?
             <DataLoading />
             :
-            sortedArrayByPrice(offerList)?.map((item, key) => (
-              <InsuranceListCard item={item} key={key} />
-            ))
+            <>
+              {
+                sortedArrayByPrice(offerList)?.map((item, key) => (
+                  <InsuranceListCard item={item} key={key} />
+                ))
+              }
+              <SkeletonShow offerCount={offerCount} offerList={offerList}/>
+            </>
         }
-
       </div>
     </div>
   )
